c9882f854eef7e6d86ebeba8e5081c555bba39e3
mumudevx <mumudevx@gmail.com>
2024-11-27 01:54:36 +0300

    hello-cargo section was added.

---
+++ 01-getting_started/02-hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+// Generated with cargo new
+// To build using that: cargo build > creates 'cargo.lock' file and '/target', '/target/debug' folders
+// 'cargo run' command will build and run executable file.
+// 'cargo check' this command quickly checks your code to make sure it compiles but doesn’t produce an executable
+// 'cargo build --release' compile it with optimizations. This command will create an executable in target/release
+
+fn main() {
+    println!("Hello, world!");
+    println!("Just another hello");
+}
